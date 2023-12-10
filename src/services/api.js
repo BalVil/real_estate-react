9664ae0d464ecf567232d39ccb841cf2ca818135
Vital <balutavili@gmail.com>
2023-12-11 00:30:47 +0200

    baseURL updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: `${process.env.REACT_APP_ARCGIS}` || "http://localhost:8000/api",
 });
 
 export const getProperties = async () => {
