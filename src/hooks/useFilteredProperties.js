af600a393c34bd9c336f1276ab8c314951344beb
Vital <balutavili@gmail.com>
2023-12-11 01:11:11 +0200

    check if filteredProperties is an array

--- src/hooks/useFilteredProperties.js
+++ src/hooks/useFilteredProperties.js
@@ -2,12 +2,16 @@ import { useMemo } from "react";
 import { commonFilter } from "../utils/common";
 
 function useFilteredProperties(data, searchValue) {
-  return useMemo(
-    () =>
-      data?.filter((property) => commonFilter([property], searchValue)[0]) ||
-      [],
-    [data, searchValue]
-  );
+  return useMemo(() => {
+    if (!Array.isArray(data)) {
+      console.error("Data is not in the expected format:", data);
+      return [];
+    }
+
+    return (
+      data?.filter((property) => commonFilter([property], searchValue)[0]) || []
+    );
+  }, [data, searchValue]);
 }
 
 export default useFilteredProperties;
