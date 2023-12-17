d3e4f01b3a459184eb8e81013a9f0b42ed1b5622
Vital <balutavili@gmail.com>
2023-12-17 21:44:58 +0200

    changed favicon place

--- src/hooks/useFilteredProperties.js
+++ src/hooks/useFilteredProperties.js
@@ -3,10 +3,10 @@ import { commonFilter } from "../utils/common";
 
 function useFilteredProperties(data, searchValue) {
   return useMemo(() => {
-    // if (!Array.isArray(data)) {
-    //   console.error("Data is not in the expected format:", data);
-    //   return [];
-    // }
+    if (!Array.isArray(data)) {
+      console.error("Data is not in the expected format:", data);
+      return [];
+    }
 
     return (
       data?.filter((property) => commonFilter([property], searchValue)[0]) || []
